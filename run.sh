eae8943b086f0af29630d39b24fd5d4ce52f4ee7
reigadegr <2722688642@qq.com>
2026-02-17 10:07:42 +0800

    chore: rename && opt run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ export RUSTFLAGS="
     -C link-arg=-fuse-ld=mold
     -C link-args=-Wl,--gc-sections,--as-needed
 "
-killall -9 pga_demo cargo timeout claude_proxy_rs >/dev/null 2>&1
+killall -9 cc_proxy pga_demo cargo timeout claude_proxy_rs >/dev/null 2>&1
 
 cargo watch -x "r" | tee target/app_log.txt 2>&1
